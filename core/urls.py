7eb3f22eedbf0fb2e13d3c0a8765b9ccd1e71dda
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-07-09 22:08:21 +0530

    fix: rename fixes

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for setup_django project.
+URL configuration for user-data-manager project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
