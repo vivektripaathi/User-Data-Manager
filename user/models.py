b195a7365c232f373a758dc587524dc53cbb6d2f
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-07-09 23:02:05 +0530

    feat(model): Create User Model - Created User Model in user app. - Generated migration file for the same

--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class UserRoleChoices(models.TextChoices):
+    ADMIN = "Admin", _("Admin")
+    USER = "User", _("User")
+
+class User(models.Model):
+    email = models.EmailField(max_length=255, primary_key=True)
+    username = models.CharField(max_length=255, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    role = models.CharField(
+        max_length=255, null=False, blank=False, choices=UserRoleChoices.choices
+    )
+    class Meta:
+        indexes = [models.Index(fields=["email"], name="pblc_usr_signp_email_idx_0001")]
+        db_table = 'users'
